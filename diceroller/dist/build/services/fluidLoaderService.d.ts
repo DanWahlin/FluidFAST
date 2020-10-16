5df88605f84adc166c2e96885dabfbcb1c155f1d
Dan Wahlin <danwahlin@iMac-Office.local>
2020-10-15 22:29:12 -0700

    remove dist

--- diceroller/dist/build/services/fluidLoaderService.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class FluidLoaderService {
-    documentId: string;
-    createNew: boolean;
-    constructor();
-    loadDataObject<T>(factory: any): Promise<T>;
-}
-//# sourceMappingURL=fluidLoaderService.d.ts.map
